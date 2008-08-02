2119be0f9ad4348846ab383efcc30a56988043fe
Cory T. Cornelius <Cory.T.Cornelius@Dartmouth.edu>
2008-08-02 01:47:07 -0400

    More friendly error message from libpcap is not present.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,8 +1,6 @@
 require 'mkmf'
 
-if have_header("pcap.h") 
-  have_library("pcap", "pcap_open_live")
-  have_library("pcap", "pcap_compile_nopcap")
-  have_library("pcap", "pcap_snapshot")
-  create_makefile("rb_pcap")
-end
+dir_config('pcap')
+fail "Couldn't find libpcap." unless have_library("pcap", "pcap_open_live", "pcap.h")
+
+create_makefile("rb_pcap")
